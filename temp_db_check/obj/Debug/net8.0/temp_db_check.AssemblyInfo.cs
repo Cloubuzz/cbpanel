81dc8ee00a76301b941e88f070d335cea1e2e2ef
Aman Dvago <aman.mansoor@dvago.pk>
2026-07-27 13:35:29 +0500

    feat: modernize outlet management interface

--- temp_db_check/obj/Debug/net8.0/temp_db_check.AssemblyInfo.cs
+++ temp_db_check/obj/Debug/net8.0/temp_db_check.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("temp_db_check")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc295e90e32ebeecba9815b579658738c1d977c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("temp_db_check")]
 [assembly: System.Reflection.AssemblyTitleAttribute("temp_db_check")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
